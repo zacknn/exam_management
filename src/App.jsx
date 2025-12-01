44d867bdc279b33141d65070e70df2cb6675d8f4
rachida <marifrachida.03@gmail.com>
2025-12-01 16:01:38 +0100

    make changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ExamLanding from "./pages/ExamLanding";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CreateExam from "./pages/CreateExam";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ExamLanding />} />
-        <Route path="/dashboard" element={<div className="p-10 text-3xl">Dashboard (Coming Soon)</div>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/auth/login" element={<Login/>} />
         <Route path="*" element={<ExamLanding />} />
+        <Route path="/createExam" element={<CreateExam/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sidebar from '../components/ui/sidebar'
+function Dashboard() {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default Dashboard
